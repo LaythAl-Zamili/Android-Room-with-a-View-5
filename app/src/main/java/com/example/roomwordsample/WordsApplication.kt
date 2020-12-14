ac44febe5a820105b41ee9cbae4a196787801bee
Layth Al-Zamili <laithalzamely@gmail.com>
2020-12-14 19:25:50 +0100

    Initial commit2

---
+++ app/src/main/java/com/example/roomwordsample/WordsApplication.kt
@@ -0,0 +1,15 @@
+package com.example.roomwordsample
+
+import android.app.Application
+
+class WordsApplication : Application() {
+    // Using by lazy so the database and the repository are only created when they're needed
+    // rather than when the application starts
+    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    val repository by lazy { WordRepository(database.wordDao()) }
+}
+//Here is what I have done:
+//
+// I created a database instance.
+// I created a repository instance, based on the database DAO.
+//Because these objects should only be created when they're first needed, rather than at app startup.
