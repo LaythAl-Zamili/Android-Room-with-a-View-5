2b09f58aa770522ba82a4be144d09a98dd26f8d1
Layth Al-Zamili <laithalzamely@gmail.com>
2020-12-14 18:57:03 +0100

    Initial commit

---
+++ app/src/main/java/com/example/roomwordsample/Word.kt
@@ -0,0 +1,17 @@
+package com.example.roomwordsample
+
+class Word {
+
+    @Entity(tableName = "word_table")
+    class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
+}
+
+annotation class ColumnInfo(val name: String)
+
+annotation class PrimaryKey
+
+annotation class Entity(val tableName: String)
+
+// Note : In the DAO (data access object), you specify SQL queries and associate them with method calls. The compiler checks the SQL and generates queries from convenience annotations for common queries, such as @Insert. Room uses the DAO to create a clean API for your code.
+//
+//The DAO must be an interface or abstract class. //Layth
